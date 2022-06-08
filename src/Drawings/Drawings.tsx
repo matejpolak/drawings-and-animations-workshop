ab7c3706fee8eda1cd05e22ceb512e9664b73a68
Matěj Polák <matej.polaak@gmail.com>
2022-06-08 16:25:36 +0200

    feat(Drawing): all steps

--- src/Drawings/Drawings.tsx
+++ src/Drawings/Drawings.tsx
@@ -14,13 +14,22 @@ const zurich = require("../assets/zurich.jpg");
 const { width, height } = Dimensions.get("window");
 export const Drawings = () => {
   const path = useValue(Skia.Path.Make());
-
   const image = useImage(zurich);
-  if (!image) {
-    return null;
-  }
-  return (
-    <Canvas style={{ flex: 1 }}>
+
+  const touchHandler = useTouchHandler({
+    onStart: ({ x, y }) => {
+      path.current.moveTo(x, y);
+    },
+    onActive: ({ x, y }) => {
+      const lastPoint = path.current.getLastPt();
+      const xMid = (lastPoint.x + x) / 2;
+      const yMid = (lastPoint.y + y) / 2;
+      path.current.quadTo(lastPoint.x, lastPoint.y, xMid, yMid);
+    },
+  });
+
+  return !image ? null : (
+    <Canvas style={{ flex: 1 }} onTouch={touchHandler}>
       <Image
         x={0}
         y={0}
