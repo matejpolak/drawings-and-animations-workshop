d4de4414393db04a965d874885f2d4d216874c21
Matěj Polák <matej.polaak@gmail.com>
2022-06-08 11:24:10 +0200

    feat: step5 bonus

--- src/AnimatedReactions/AnimatedReactions.tsx
+++ src/AnimatedReactions/AnimatedReactions.tsx
@@ -2,15 +2,9 @@ import React, { useState } from "react";
 import Animated, {
   withTiming,
   useAnimatedStyle,
-  useSharedValue,
-  Easing,
-  withSpring,
-  withSequence,
-  useAnimatedReaction,
-  withRepeat,
-  BounceIn,
-  BounceOut,
   ZoomOut,
+  Keyframe,
+  ZoomIn,
 } from "react-native-reanimated";
 import { Pressable } from "react-native";
 import Icon from "@expo/vector-icons/MaterialIcons";
@@ -22,15 +16,43 @@ const AnimatedIcon = Animated.createAnimatedComponent(Icon);
 const ACTIVE_COLOR = "#f3acac";
 const DEFAULT_COLOR = "#6c6b6b";
 
+const keyframe = new Keyframe({
+  0: {
+    transform: [
+      { rotate: "0deg" },
+      {
+        scale: 0.5,
+      },
+    ],
+  },
+  33: {
+    transform: [
+      { rotate: "-15deg" },
+      {
+        scale: 1.25,
+      },
+    ],
+  },
+  66: {
+    transform: [
+      { rotate: "15deg" },
+      {
+        scale: 1.5,
+      },
+    ],
+  },
+  100: {
+    transform: [
+      { rotate: "0deg" },
+      {
+        scale: 1,
+      },
+    ],
+  },
+});
+
 function Heart() {
   const [selected, setSelected] = useState(false);
-
-  const animatedProps = useAnimatedStyle(
-    () => ({
-      color: withTiming(selected ? ACTIVE_COLOR : DEFAULT_COLOR),
-    }),
-    [selected]
-  );
 
   return (
     <Pressable
@@ -40,11 +62,11 @@ function Heart() {
     >
       <AnimatedIcon
         key={`Animated-${selected}`}
-        entering={BounceIn}
+        entering={selected ? keyframe.duration(300) : ZoomIn}
         exiting={ZoomOut}
         name={"favorite"}
         size={50}
-        animatedProps={animatedProps}
+        color={selected ? ACTIVE_COLOR : DEFAULT_COLOR}
       />
     </Pressable>
   );
