f5a990e71600c5115ddd0ff5d7113a60e7bdcd2a
Matěj Polák <matej.polaak@gmail.com>
2022-06-08 10:59:49 +0200

    feat: step4 + bonus

--- src/AnimatedReactions/AnimatedReactions.tsx
+++ src/AnimatedReactions/AnimatedReactions.tsx
@@ -6,6 +6,8 @@ import Animated, {
   Easing,
   withSpring,
   withSequence,
+  useAnimatedReaction,
+  withRepeat,
 } from "react-native-reanimated";
 import { Pressable } from "react-native";
 import Icon from "@expo/vector-icons/MaterialIcons";
@@ -17,14 +19,18 @@ const AnimatedIcon = Animated.createAnimatedComponent(Icon);
 const ACTIVE_COLOR = "#f3acac";
 const DEFAULT_COLOR = "#6c6b6b";
 
+const WOBBLE_START = "0deg";
+const WOBBLE_END = "15deg";
+
 function Heart() {
   const [selected, setSelected] = useState(false);
+  const wobble = useSharedValue(WOBBLE_START);
 
   const styles = useAnimatedStyle(
     () => ({
-      transform: [{ scale: withSpring(selected ? 2 : 1) }],
+      transform: [{ rotate: wobble.value }],
     }),
-    [selected]
+    [wobble]
   );
 
   const animatedProps = useAnimatedStyle(
@@ -32,6 +38,26 @@ function Heart() {
       color: withTiming(selected ? ACTIVE_COLOR : DEFAULT_COLOR),
     }),
     [selected]
+  );
+
+  useAnimatedReaction(
+    () => selected,
+    (value) => {
+      if (value) {
+        wobble.value = withRepeat(
+          withSequence(
+            withTiming(WOBBLE_END, {
+              duration: 50,
+            }),
+            withTiming(WOBBLE_START, {
+              duration: 50,
+            })
+          ),
+          3,
+          true
+        );
+      }
+    }
   );
 
   return (
@@ -43,8 +69,8 @@ function Heart() {
       <AnimatedIcon
         name={"favorite"}
         size={50}
-        style={styles}
         animatedProps={animatedProps}
+        style={styles}
       />
     </Pressable>
   );
